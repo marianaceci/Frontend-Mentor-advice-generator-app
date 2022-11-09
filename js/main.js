13985df5b5480f7fdf12b1a7b0d2d7c0a76b8036
Mariana Schmidt <marianaceciliaschmidt@gmail.com>
2022-11-08 21:27:23 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.querySelector('button').addEventListener('click', getAdvice)
+
+function getAdvice() {
+    fetch('https://api.adviceslip.com/advice')
+        .then(res => res.json())
+        .then(data => {
+            //console.log(data.slip.advice)
+            //console.log(data.slip.id)
+
+            document.getElementById('adviceText').innerText = data.slip.advice
+            document.getElementById('adviceId').innerText = data.slip.id
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
+        });
+}
